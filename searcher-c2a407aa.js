97920751cec52caba882e8429c7f7188c38d5699
byteappua <byteappua@users.noreply.github.com>
2026-01-19 03:12:22 +0000

    deploy: 55153f66d0ffd6ad05f567c98dbd9614d899f7fb

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-507a4bb0.js',
+                path_to_root + 'searchindex-83b4c1b2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
